43d171a81634b11fa064ecdfe52a5cab9179c98b
Michael Bee <bee.michael.02@gmail.com>
2022-02-12 23:45:09 -0600

    All working, except also adding object HTMLTextAreaElement to the text box.

--- script.js
+++ script.js
@@ -60,50 +60,49 @@ var num = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var special = ["!", "@", "#", "?", "%", "*"];
 var passChar = [];
 
-function writePassword() {
-  //is this part the problem? vvvvv
-  var password = generatePassword();
+function generatePassword() {
+  var numChar = prompt("How long should your password be? (Please choose a number between 8-128");
+  if (numChar < 8 || numChar > 128 || numChar == null) {
+    alert("Invalid entry. Please pick a number between 8-128.");
+    generatePassword();
+    } else console.log(numChar);
 
-  //All of this appears to work vvvvv
-  function generatePassword() {
-    var numChar = prompt("How long should your password be? (Please choose a number between 8-128");
-    if (numChar < 8 || numChar > 128 || numChar == null) {
-      alert("Invalid entry. Please pick a number between 8-128.");
-      generatePassword();
-      } else console.log(numChar);
-
-    var incLC = confirm("Would you like to include lowercase characters?");
-    if (incLC === true) {
-      passChar = passChar.concat(lowercase);
-    }
-    var incUC = confirm("Would you like to include uppercase characters?");
-    if (incUC === true) {
-      passChar = passChar.concat(uppercase);
-    }
-    var incNum = confirm("Would you like to include numbers?");
-    if (incNum === true) {
-      passChar = passChar.concat(num);
-    }
-    var incSC = confirm("Would you like to include special characters?");
-    if (incSC === true) {
-      passChar = passChar.concat(special);
-    }
-    if (passChar.length === 0) {
-      console.log("Array is empty.");
-      alert("You can't generate a password if you don't select any characters. Please try again.");
-    }
-    console.log(passChar);
-    //All of this part seems to work ^^^^^^^^^^   
-  
-    for (var i = 0; i < numChar; i++) {
-      password += passChar[Math.floor(Math.random() * passChar.length)];
-    }
-    console.log(password);
-    return password;
+  var incLC = confirm("Would you like to include lowercase characters?");
+  if (incLC === true) {
+    passChar = passChar.concat(lowercase);
   }
+  var incUC = confirm("Would you like to include uppercase characters?");
+  if (incUC === true) {
+    passChar = passChar.concat(uppercase);
+  }
+  var incNum = confirm("Would you like to include numbers?");
+  if (incNum === true) {
+    passChar = passChar.concat(num);
+  }
+  var incSC = confirm("Would you like to include special characters?");
+  if (incSC === true) {
+    passChar = passChar.concat(special);
+  }
+  if (passChar.length === 0) {
+    console.log("Array is empty.");
+    alert("You can't generate a password if you don't select any characters. Please try again.");
+  }
+
+  console.log(passChar);
+  console.log(numChar);
+  
+  for (var i = 0; i < numChar; i++) {
+    password += passChar[Math.floor(Math.random() * passChar.length)];
+  }
+
+  console.log(password);
+  return password;
 }
 
+function writePassword() {
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-      passwordText.value = password;
+  passwordText.value = password;
+}
 
 generateBtn.addEventListener("click", writePassword);
